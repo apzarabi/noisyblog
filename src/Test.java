0cbdf4481e98adec1025fc5332d6f8f834e64901
Mohamad Latifian <mohamad.latifian@gmail.com>
2015-04-08 15:41:25 +0430

    varzesh3 added

--- src/Test.java
+++ src/Test.java
@@ -15,7 +15,7 @@ public class Test {
 	//the first line should show the target page, 
 	//and all other line should be url to post for building StyleTree
 	//there will be an output.html and output.txt representing the output
-	static String sourceFolderPath = "tests/chenan.blogfa.com";
+	static String sourceFolderPath = "tests/varzesh3.com";
 	static ArrayList<String> sources = new ArrayList<String>();
 	static String target;
 
